798b655c009d02bd55dcc680188dee981292db8c
ira <ira.khodeeva.81@mal.ru>
2025-12-06 18:24:00 +0500

    SQLAlchemy

---
+++ 09_lesson/models_student.py
@@ -0,0 +1,10 @@
+# models.py
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Student(Base):
+    __tablename__ = 'students'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
